412349def222b5b841c83a28bb642cd18cfd3fd9
chidanand <chidanandnh4877@gmail.com>
2025-11-10 14:44:18 +0530

    10/11/25_V6

--- src/test/java/practice_package_01/Practice_class_01.java
+++ src/test/java/practice_package_01/Practice_class_01.java
@@ -4,7 +4,7 @@ public class Practice_class_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Package 1");
+		System.out.println("Package 11");
 
 	}
 
