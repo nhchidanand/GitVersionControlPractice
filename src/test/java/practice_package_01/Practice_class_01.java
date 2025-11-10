824b7bb80c3e3d084abfe47a4e1578291f564d68
chidanand <chidanandnh4877@gmail.com>
2025-11-10 13:46:44 +0530

    10/11/25_V1

---
+++ src/test/java/practice_package_01/Practice_class_01.java
@@ -0,0 +1,11 @@
+package practice_package_01;
+
+public class Practice_class_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Package 1");
+
+	}
+
+}
---
+++ src/test/java/practice_package_02/Practice_class_02.java
@@ -0,0 +1,9 @@
+package practice_package_02;
+
+public class Practice_class_02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Package 2");
+
+	}
+}
