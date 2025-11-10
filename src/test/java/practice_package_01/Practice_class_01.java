f5748963a2a3ecb1cae4f50fd9cee50de034d1b6
chidanand <chidanandnh4877@gmail.com>
2025-11-10 14:50:51 +0530

    10/11/25_V8

--- src/test/java/practice_package_01/Practice_class_01.java
+++ src/test/java/practice_package_01/Practice_class_01.java
@@ -4,7 +4,7 @@ public class Practice_class_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Package JAVA");
+		System.out.println("Package JAVA_SELENIUM");
 
 	}
 
